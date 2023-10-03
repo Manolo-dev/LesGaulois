124f78aa1e902c1c40206256aab374c5bbb2964f
Manolo-dev <sardo.manolo.dev@gmail.com>
2023-10-03 17:29:22 +0200

    enum Equipement

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -5,6 +5,7 @@ public class Romain {
 		this.nom = nom;
 		assert force >= 0;
 		this.force = force;
+		this.equipements = new Equipement[2];
 	}
 	
 	public String getNom() {
@@ -17,6 +18,23 @@ public class Romain {
 
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "«" + texte + "»");
+	}
+	
+	public void sEquiper(Equipement equip) {
+		switch(nbEquipement) {
+		case 1:
+			if(equip == equipements[0]) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equip);
+				break;
+			}
+		case 0:
+			equipements[0] = equip;
+			System.out.println("Le soldat " + nom + " s'équipe avec un " + equip);
+			nbEquipement++;
+			break;
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+		}
 	}
 	
 	public void recevoirCoup(int forceCoup) {
@@ -32,12 +50,15 @@ public class Romain {
 	}
 
 	public static void main(String[] args) {
-		Romain bonus = new Romain("Caius Bonus", 6);
-		System.out.println(bonus.prendreParole());
-		bonus.parler("Salut !");
-		bonus.recevoirCoup(10);
+		Romain minus = new Romain("Minus", 6);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.BOUCLIER);
 	}
 	
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement = 0;
 }
